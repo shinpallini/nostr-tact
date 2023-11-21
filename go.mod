211e63d9181f4dc671b2576b55ad8182af16d125
pallini <shinpall1ni@gmail.com>
2023-11-21 23:22:57 +0900

    update: display username using pubkey

--- go.mod
+++ go.mod
@@ -1,19 +1,38 @@
 module nostrtact
 
-go 1.21.3
+go 1.20
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/mattn/go-runewidth v0.0.15
+	github.com/nbd-wtf/go-nostr v0.25.7
+	github.com/rivo/tview v0.0.0-20231115183240-7c9e464bac02
+)
+
+require (
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/dgraph-io/ristretto v0.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell/v2 v2.6.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.2.0 // indirect
+	github.com/golang/glog v1.0.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/rivo/tview v0.0.0-20231115183240-7c9e464bac02 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/puzpuzpuz/xsync/v2 v2.5.1 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/urfave/cli/v2 v2.25.7 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/term v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,20 +2,79 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/signal"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/mattn/go-runewidth"
+	"github.com/nbd-wtf/go-nostr"
 	"github.com/rivo/tview"
 )
 
+type metadataContent struct {
+	Name        string `json:"name"`
+	DispalyName string `json:"display_name"`
+}
+
 func main() {
+	// sk := nostr.GeneratePrivateKey()
+	// pk, _ := nostr.GetPublicKey(sk)
 	app := tview.NewApplication()
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
+
+	// リレーサーバーの設定
+	relay, err := nostr.RelayConnect(ctx, "wss://relay-jp.nostr.wirednet.jp")
+	if err != nil {
+		panic(err)
+	}
+	filters := []nostr.Filter{{
+		Kinds: []int{nostr.KindTextNote},
+		Limit: 1,
+	}}
+
+	sub, err := relay.Subscribe(ctx, filters)
+	if err != nil {
+		panic(err)
+	}
+
+	userNames := func() map[string]string {
+		m := make(map[string]string)
+		filters := []nostr.Filter{{
+			Kinds: []int{nostr.KindProfileMetadata},
+			Limit: 10000,
+		}}
+		sub, err := relay.Subscribe(ctx, filters)
+		if err != nil {
+			panic(err)
+		}
+		for {
+			select {
+			case ev := <-sub.Events:
+				var content metadataContent
+				err = json.Unmarshal([]byte(ev.Content), &content)
+				if err != nil {
+					// panic(err)
+					continue
+				}
+				m[ev.PubKey] = content.DispalyName
+			case <-time.After(2 * time.Second):
+				return m
+			case <-ctx.Done():
+				return m
+			}
+		}
+	}()
+	f, err := os.Create("length.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	fmt.Fprintf(f, "user count: %d", len(userNames))
 
 	// 名前、メッセージ、時刻用のTextViewを作成
 	nameView := tview.NewTextView().SetDynamicColors(true)
@@ -25,29 +84,32 @@ func main() {
 	// レイアウト用のFlexを作成し、3つのTextViewを追加
 	flex := tview.NewFlex().
 		AddItem(nameView, 0, 1, false).
-		AddItem(messageView, 0, 2, false).
-		AddItem(timeView, 0, 1, false)
+		AddItem(messageView, 0, 4, false).
+		AddItem(timeView, 0, 2, false)
 
+	var mu sync.Mutex
 	go func(ctx context.Context) {
-		for i := 0; ; i++ {
+		for ev := range sub.Events {
 			select {
 			case <-ctx.Done():
 				return
 			default:
-				time.Sleep(1 * time.Second)
 				app.QueueUpdateDraw(func() {
 					// 現在のテキストを取得
 					nameText := nameView.GetText(true)
 					messageText := messageView.GetText(true)
 					timeText := timeView.GetText(true)
 
-					newText := runewidth.Wrap(fmt.Sprintf("あああああああああああ %d", i), 20)
+					newText := runewidth.Wrap(fmt.Sprintf(ev.Content), 77)
 					paddingLines := strings.Repeat("\n", len(strings.Split(newText, "\n"))-1)
 
 					// 新しいテキストを設定
-					nameView.SetText(fmt.Sprintf("Name %d\n%s%s", i, paddingLines, nameText))
+					mu.Lock()
+					nameView.SetText(fmt.Sprintf("%s\n%s%s", userNames[ev.PubKey], paddingLines, nameText))
+					mu.Unlock()
 					messageView.SetText(fmt.Sprintf("%s\n%s", newText, messageText))
-					timeView.SetText(fmt.Sprintf("%s\n%s%s", time.Now().Format("15:04:05"), paddingLines, timeText))
+					timeView.SetText(fmt.Sprintf("%s\n%s%s", ev.CreatedAt.Time(), paddingLines, timeText))
+					// time.Sleep(1 * time.Second)
 				})
 			}
 		}
@@ -57,3 +119,11 @@ func main() {
 		panic(err)
 	}
 }
+
+// func getMetadata() map[string]string {
+// 	filters := []nostr.Filter{{
+// 		Kinds: []int{nostr.KindProfileMetadata},
+// 		Limit: 1,
+// 	}}
+
+// }
